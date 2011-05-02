5416815a1c7a6cc831937bc89efe3d111c7f7ab3
lfborjas <luis.borjas@escolarea.com>
2011-05-02 12:07:33 -0600

    creadas urls

--- stories/urls.py
+++ stories/urls.py
@@ -1,4 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('stories.views',
+
+   url(r'^new$', 'create', name="create"),
+   url(r'^$', 'all', name="all_stories"),
+   url(r'^open', 'all', {'open': True},name="add"),
+   url(r'^closed', 'all', {'open': False}, name="read"),
+
+   url(r'^(?P<story_id>\d+)$', 'show', name="show_story"),
+   url(r'^(?P<story_id>\d+)/full$', 'show', {'full': True}, name="read_story"),
+   url(r'^(?P<story_id>\d+)/continue$', 'append', name="continue_story")
 )

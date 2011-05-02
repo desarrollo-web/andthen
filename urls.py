a62b519bbf371186d5f499a1770179d4eb18e085
lfborjas <luis.borjas@escolarea.com>
2011-05-01 23:42:44 -0600

    creada app de stories y template base

--- urls.py
+++ urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
-
+from django.views.generic.simple import direct_to_template
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'nerdit.views.home', name='home'),
-    # url(r'^nerdit/', include('nerdit.foo.urls')),
+    url(r'^$', direct_to_template, {'template': 'index.html'}, name='home'),
+    (r'^stories/', include('stories.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),

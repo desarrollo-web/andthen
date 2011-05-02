b92719a780d52e881fc7cac18e1ec3db4aceb343
lfborjas <luis.borjas@escolarea.com>
2011-05-02 12:22:22 -0600

    escondiendo el related field

--- stories/forms.py
+++ stories/forms.py
@@ -1,7 +1,8 @@
-from django.forms import ModelForm
-from stories.models import Paragraph
+from stories.models import Paragraph, Story
+from django import forms
 
-class ParagraphForm(ModelForm):
+class ParagraphForm(forms.ModelForm):
+    story = forms.ModelChoiceField(queryset=Story.objects.all(), widget=forms.HiddenInput(), required=False)
     class Meta:
         model = Paragraph
-        exclude = ('created_at', 'upvotes')
+        exclude = ('created_at', 'upvotes', 'story')
